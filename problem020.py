32ace50d8ed9a60fa5079d3f6d41faa5271cdd51
iShowCode <38829825+MrGusain@users.noreply.github.com>
2022-05-15 12:36:00 +0530

    Update problem020.py

--- problem020.py
+++ problem020.py
@@ -11,24 +11,9 @@ Find the sum of the digits in the number 100!
 '''
 
 
-# def factorial_digit_sum(num):
-#     factorial = sum(int(x) for x in str(reduce(lambda x,y : x * y ,range(1,num+1))))
-#     print(factorial)
+def factorial_digit_sum(num):
+    factorial = sum(int(x) for x in str(reduce(lambda x,y : x * y ,range(1,num+1))))
+    print(factorial)
 
 
-# factorial_digit_sum(100)
-
-
-def amicable_numbers(num):
-    a = 1 + sum(nu for nu in range(2,num//2+2) if num%nu == 0)
-    # print(a,1 + sum(nu for nu in range(2,a//2+2) if a%nu == 0))
-    if a == num:
-        return False
-    return True if (1 + sum(nu for nu in range(2,a//2+2) if a%nu == 0)) == num else False
-
-
-# print(amicable_numbers(220))
-
-total = sum(x for x in range(10000) if amicable_numbers(x))
-print(total)
-
+factorial_digit_sum(100)
