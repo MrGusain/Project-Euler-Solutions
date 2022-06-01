f4e762b8e8df47fe4e04e96d88b3425af8c9fc0a
iShowCode <38829825+MrGusain@users.noreply.github.com>
2022-06-01 22:20:25 +0530

    Update problem003.py

--- problem003.py
+++ problem003.py
@@ -7,14 +7,41 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 '''
 
-def all_prime_factors(num):
-    factors,start = [1],2
-    limit = int(math.sqrt(num))+3
+def allPrimeFactors(n):
+    factors = []
+    if n % 2 == 0:
+        lastFactor = 2
+        n = n // 2
+        factors.append(2)
+        while n % 2 == 0:
+            n = n // 2
+            factors.append(2)
+    else:
+        lastFactor = 1
 
-    while start <= limit:
-        if num%start == 0:
-            factors.append(start)
-            num //= start
-        else:
-            start += 2 if start != 2 else 1
+    factor = 3
+    maxFactor = int(math.sqrt(n)) + 1
+    while n>1 and factor <= maxFactor:
+        if n % factor == 0:
+            factors.append(factor)
+            n = n // factor
+            lastFactor = factor
+            while n % factor == 0:
+                factors.append(factor)
+                n = n // factor
+            maxFactor = int(math.sqrt(n)) + 1
+
+        factor += 2
+
+
+    if n != 1:
+        factors.append(n) 
+
     return factors
+
+n = 600851475143
+factors = allPrimeFactors(n)
+print(factors)
+print(max(factors))
+
+
