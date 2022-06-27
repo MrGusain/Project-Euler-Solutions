17c1ea8053a5c7ece4c1f2a831596e1314922336
MrGusain <bhanupratapsinghgusain1@gmail.com>
2022-06-27 21:09:54 +0530

    48 solution

--- problem048.py
+++ problem048.py
@@ -2,9 +2,9 @@
 Self powers
 Problem 48
 
-The series, 11 + 22 + 33 + ... + 1010 = 10405071317.
+The series, 1¹ + 2² + 3³ + ... + 10¹⁰ = 10405071317.
 
-Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.
+Find the last ten digits of the series, 1¹ + 2² + 3³ + ... + 1000¹⁰⁰⁰.
 '''
 
 def lastNdigits(n = 10):
